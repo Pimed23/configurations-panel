ec0ddbd4166a735b7383101f81348baac4f2dcd5
pimed23 <b.pimed@gmail.com>
2022-09-27 13:10:09 -0500

    Estructura inicial del aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Panel de Configuración</h1>
+      <h3>Panel de control para administrar configuraciones</h3>
+      <form>
+        <div>
+          <label>Nombre de la empresa</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Tipo de la empresa</label>
+          <select>
+            <option value='dlv'>Delivery</option>
+            <option value='sl'>Social Listening</option>
+            <option value='msj'>Mensajería</option>
+          </select>
+        </div>
+        <div>
+          <label>Flujo</label>
+          <select>
+            <option value=''></option>
+          </select>
+        </div>
+        <div>
+          <label>Auto Respuesta</label>
+          <select>
+            <option value=''></option>
+          </select>
+        </div>
+        <div>
+          <button type='submit'>Enviar</button>
+        </div>
+      </form>
     </div>
   );
 }
